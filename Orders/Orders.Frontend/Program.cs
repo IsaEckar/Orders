d109ccc48413b030f85c6d6a41967e6c84c7c6ca
IsauraEckar <isauraEckar@gmail.com>
2024-03-02 13:39:49 -0500

    Add a point to the error messages and see the countries in the view

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
+using Orders.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//unir backend con frontend con addscoped
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7031/") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
